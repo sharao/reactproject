5e1f90689d505104699d8b17bd731deb4f98f5f9
sharao <sharao111@gmail.com>
2021-06-15 08:40:30 +0530

    error fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Expenses from './components/Expenses/Expenses'
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense'
 
 function App() {
   const expenses = [
@@ -24,7 +25,7 @@ function App() {
 ];
   return (
     <div className="App">
-      <div>Let's get started!</div>
+      <NewExpense/>
       <Expenses items = {expenses}></Expenses>
     </div>
   );
